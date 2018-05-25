8f3cb9265849a7cd4c73cdfc4bee1c39fc06d31b
Gul Mohammed <isahil@MacBook-Pro-3.local>
2018-05-25 13:20:57 +0530

    final commit

---
+++ play/app/controllers/GitDemo.java
@@ -0,0 +1,11 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class GitDemo extends Controller{
+	public static void  dummy() {
+		System.out.println("Hello World");
+		render();
+	}
+	
+}
